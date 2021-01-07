e0884668e7491b99e1070d8e9e4fc848b7027c58
Donald Vu <donaldhdvu@gmail.com>
2021-01-07 17:04:19 -0600

    validation practice

--- mongooseBasics/index.js
+++ mongooseBasics/index.js
@@ -18,14 +18,14 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model('Movie', movieSchema)
 // const batman = new Movie({ title: 'Batman', year: 2006, score: 9.0, rating: 'R' })
 
-Movie.insertMany([
-    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-    { title: 'Rush Hour', year: 2000, score: 9.6, rating: 'PG' }
-])
-    .then(data => {
-        console.log("IT WORKED");
-        console.log(data);
-    })
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Rush Hour', year: 2000, score: 9.6, rating: 'PG' }
+// ])
+//     .then(data => {
+//         console.log("IT WORKED");
+//         console.log(data);
+//     })
---
+++ mongooseBasics/product.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!");
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!");
+        console.log(err);
+    })
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price must be positive']
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: {
+        type: [String],
+        default: 'Cycling'
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L']
+    }
+})
+const Product = mongoose.model('Product', productSchema)
+
+const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'XS' })
+bike.save()
+    .then(data => {
+        console.log("IT WORKED");
+        console.log(data);
+    })
+    .catch(err => {
+        console.log("OH NO ERROR");
+        console.log(err);
+    })
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9.99 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log("IT WORKED");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO ERROR");
+//         console.log(err);
+//     })
