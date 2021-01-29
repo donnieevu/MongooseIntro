502b1326b66a38725cf644b3bd5e5ef36222984a
Donny Vu <donnyvu@donnys-mbp.lan>
2021-01-29 04:22:48 -0600

    Practining pre/post hooks, middlewarea

--- mongooseBasics/person.js
+++ mongooseBasics/person.js
@@ -17,4 +17,13 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
         return `${this.first} ${this.last}`
     })
 
+    personSchema.pre('save', async function(){
+        this.first = 'YO'
+        this.last = 'MOMMA'
+        console.log('ABOUT TO SAVE');
+    })
+    personSchema.post('save', async function(){
+        console.log('JUST SAVED');
+    })
+
     const Person = mongoose.model('Person', personSchema)
